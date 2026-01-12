c9dccadf81aeeffe59bd24c95db5bcadafa3fe19
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-01-11 19:38:49 -0600

    add initial migrations

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260112_013837 from './20260112_013837';
+
+export const migrations = [
+  {
+    up: migration_20260112_013837.up,
+    down: migration_20260112_013837.down,
+    name: '20260112_013837'
+  },
+];
