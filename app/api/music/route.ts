57de6553a4a8294a738e924f5bfe59026c55556b
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-01-16 19:35:32 -0600

    Add refresh token logic for Spotify API

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -3,14 +3,70 @@ import { NextResponse } from 'next/server';
 let cache: { name: string | null, artist: string | null, playing: boolean | null, position: number | null, duration: number | null } | null = null;
 let expiresAt: number | null = null;
 
-// TO-DO: Refresh token logic agggh
-async function getCurrentTrack() {
+let accessToken: string | null = null;
+let tokenExpiresAt: number | null = null;
+
+// Helper to refresh the Spotify access token
+async function refreshAccessToken() {
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+    const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+
+    if (!refreshToken || !clientId || !clientSecret) {
+        console.warn('Missing Spotify refresh token or client credentials.');
+        return null;
+    }
+
+    const req = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken
+        }),
+    });
+
+    try {
+        const res = await req.json();
+
+        if (res.access_token) {
+            accessToken = res.access_token;
+            tokenExpiresAt = Date.now() + (res.expires_in * 1000);
+            return accessToken;
+        } else {
+            return null;
+        }
+    } catch {
+        return null;
+    }
+}
+
+async function getCurrentTrack(retry = false) {
+    if (!accessToken || (tokenExpiresAt && Date.now() >= tokenExpiresAt)) await refreshAccessToken(); // attempt refresh
+    if (!accessToken) return null; // fail on missing token
+
     const req = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
         headers: {
-            "Authorization": `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
+            "Authorization": `Bearer ${accessToken}`
         },
         cache: "no-store"
     });
+
+    // if unauthorized try to refresh token and retry again
+    if (req.status === 401) {
+        if (retry === true) return null; // avoiding infinite loops
+
+        try {
+            await refreshAccessToken();
+        } catch {
+            return null;
+        }
+        
+        return getCurrentTrack(true); // retry once
+    }
 
     if (req.status === 204 || !req.ok) return null;
 
