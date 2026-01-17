ad4493c5ffb912ad9effb3b0cf0a29494e7faba8
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-01-17 15:52:02 -0600

    Add blog page to display posts (not done yet)

---
+++ app/(app)/blog/page.tsx
@@ -0,0 +1,69 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import Link from 'next/link';
+
+import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
+import { convertLexicalToPlaintext } from '@payloadcms/richtext-lexical/plaintext'
+
+function extractText(body: any): string {
+    try {
+        const data: SerializedEditorState = body;
+        return convertLexicalToPlaintext({ data });
+    } catch {
+        return '';
+    }
+}
+
+function formatDate(str: string): string {
+    const date = new Date(str);
+
+    // this should give us a format like Jan 17, 2026
+    return date.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    });
+}
+
+function calcReadTime(text: string): string {
+    if (!text) return '1 min read';
+
+    const words = (text.match(/\b\w+\b/g) || []).length; // word count
+    return `${Math.max(1, Math.ceil(words / 238))} min read`;
+}
+
+export default async function Blog() {
+    const payload = await getPayload({ config });
+
+    // will fix later, this is temporary to show some posts
+    const page = 1;
+    const totalPages = 67;
+
+    const posts = await payload.find({
+        collection: 'posts',
+        limit: 12,
+        page: page,
+        sort: '-createdAt'
+    });
+
+    return (
+        <main className="max-w-5xl space-y-6 mt-20">
+            <div className="flex items-center justify-between">
+                <h1 className="text-4xl font-semibold">Blog</h1>
+                <p className="text-sm font-medium text-zinc-400 ">Showing page {page} of {totalPages}</p>
+            </div>
+
+            <section className="grid grid-cols-1 gap-6">
+                {posts?.docs?.map((post: any) => (
+                    <Link key={post.id} href={`/blog/${post.slug}`} className="block">
+                        <article className="border border-white/10 bg-zinc-900 rounded-xl p-5 hover:bg-zinc-800 transition-colors">
+                            <h2 className="text-2xl font-bold mb-2 line-clamp-2">{post.title}</h2>
+                            <div className="text-zinc-400 text-sm mb-2">{formatDate(post.createdAt)} <span className="font-bold mx-1">•</span> {calcReadTime(extractText(post.body))}</div>
+                            <p className="text-zinc-400 font-medium mb-4 line-clamp-3">{extractText(post.body)}</p>
+                        </article>
+                    </Link>
+                ))}
+            </section>
+        </main>
+    )
+}
