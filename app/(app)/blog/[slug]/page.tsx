7b641c515ad304bf83c45b0c018991ad5340e168
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-01-18 14:03:58 -0600

    Switch to `@tailwindcss/typography`.

--- app/(app)/blog/[slug]/page.tsx
+++ app/(app)/blog/[slug]/page.tsx
@@ -3,41 +3,8 @@ import config from '@payload-config'
 
 import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
 import { RichText as RichTextConverter } from '@payloadcms/richtext-lexical/react'
-import { defaultJSXConverters } from '@payloadcms/richtext-lexical/react';
 import { extractText, formatDate, calcReadTime } from '@/lib/utils';
 import { redirect } from 'next/navigation';
-
-const converters = {
-    ...defaultJSXConverters,
-    heading: ({ node, nodesToJSX }: { node: any; nodesToJSX: any }) => {
-        const classes = {
-            h1: 'text-4xl font-bold mt-8 mb-4',
-            h2: 'text-3xl font-bold mt-7 mb-3',
-            h3: 'text-2xl font-bold mt-6 mb-3',
-            h4: 'text-xl font-bold mt-5 mb-2',
-            h5: 'text-lg font-bold mt-4 mb-2',
-            h6: 'text-base font-bold mt-4 mb-2',
-        };
-        
-        const children = nodesToJSX({ nodes: node.children });
-        const Tag = node.tag as keyof typeof classes;
-        
-        return (
-            <Tag className={classes[Tag]}>
-                {children}
-            </Tag>
-        );
-    },
-    paragraph: ({ node, nodesToJSX }: { node: any; nodesToJSX: any }) => {
-        const children = nodesToJSX({ nodes: node.children });
-
-        return (
-            <p className="mb-4 text-xl font-medium leading-relaxed">
-                {children}
-            </p>
-        );
-    },
-};
 
 export default async function Blog({ params }: { params?: { slug?: string } }) {
     const payload = await getPayload({ config });
@@ -62,13 +29,13 @@ export default async function Blog({ params }: { params?: { slug?: string } }) {
     if (!post) redirect(`/blog`);
 
     return (
-        <article className="max-w-5xl mt-20">
+        <article className="prose max-w-5xl mt-12">
             <header className="mb-6">
-                <h1 className="text-5xl font-bold mb-4">{post.title}</h1>
+                <h1 className="title">{post.title}</h1>
                 <div className="text-zinc-300 text-xl font-semibold mb-2">Published on {formatDate(post.createdAt)} <span className="font-bold mx-1">•</span> {calcReadTime(extractText(post.body))}</div>
             </header>
 
-            <RichTextConverter converters={converters} data={post.body as SerializedEditorState} />
+            <RichTextConverter data={post.body as SerializedEditorState} />
         </article>
     )
 }
