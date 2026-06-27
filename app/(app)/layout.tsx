c870af6fe0ce399ac154cba8f87967d8290f8ff8
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-06-27 14:47:28 -0500

    Add favicon from GitHub profile.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 			<head>
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" href="https://avatars.githubusercontent.com/u/72092838?v=4" type="image/png" />
 				<Analytics />
 			</head>
 			<body className="min-h-screen flex flex-col bg-backdrop text-text">
