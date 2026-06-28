680fc17c22cdbb8d8948040fe0ec9d5aea73d391
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-06-28 14:30:28 -0500

    Create icon files from skills for reusability.

---
+++ components/icons/AdobePhotoshop.tsx
@@ -0,0 +1,9 @@
+import { HTMLAttributes } from "react";
+
+export default function AdobePhotoshop({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) {
+    return (
+        <h1 className={`font-extrabold text-header${className ? ` ${className}` : ""}`} {...props}>
+            Ps
+        </h1>
+    );
+}
