5ab1f7811c7570237653d7bd45153a54d43028b6
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-06-28 15:00:42 -0500

    Move skills data to constants and map in `Skills.tsx`.

--- app/(app)/Skills.tsx
+++ app/(app)/Skills.tsx
@@ -1,29 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
-// -- Icons -- //
-import React from "@/components/icons/React";
-import Typescript from "@/components/icons/Typescript";
-import Javascript from "@/components/icons/Javascript";
-import HTML5 from "@/components/icons/HTML5";
-import CSS from "@/components/icons/CSS";
-import EJS from "@/components/icons/EJS";
-import Tailwind from "@/components/icons/Tailwind";
-import NodeJS from "@/components/icons/NodeJS";
-import Python from "@/components/icons/Python";
-import Java from "@/components/icons/Java";
-import Express from "@/components/icons/Express";
-import Prisma from "@/components/icons/Prisma";
-import Discord from "@/components/icons/Discord";
-import Bash from "@/components/icons/Bash";
-import Arduino from "@/components/icons/Arduino";
-import Docker from "@/components/icons/Docker";
-import Blender from "@/components/icons/Blender";
-import Fusion360 from "@/components/icons/Fusion360";
-import VSCode from "@/components/icons/VSCode";
-import AdobePhotoshop from "@/components/icons/AdobePhotoshop";
-import AdobeIllustrator from "@/components/icons/AdobeIllustrator";
+import { SKILLS_DATA } from "@/lib/constants";
 
 const FADE_DURATION = 200;
 
@@ -82,127 +60,12 @@ export default function Skills() {
 					transform: phase === "exit" ? "translateY(10px)" : "translateY(0)",
 				}}
 			>
-				{skillsTab === "frontend" && (
-					<>
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<React className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">React / Native</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Typescript className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Typescript</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Javascript className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Javascript</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<HTML5 className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">HTML5</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<CSS className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">CSS</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<EJS className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">EJS</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Tailwind className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Tailwind CSS</span>
-						</div>
-					</>
-				)}
-
-				{skillsTab === "backend" && (
-					<>
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<NodeJS className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Node.js</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Typescript className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Typescript</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Python className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Python</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Java className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Java</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Express className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Express</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Prisma className="size-9 my-0.5 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Prisma</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Discord className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Discord.js</span>
-						</div>
-					</>
-				)}
-
-				{skillsTab === "software" && (
-					<>
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Bash className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Bash</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Arduino className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Arduino</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Docker className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Docker</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Blender className="size-10 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Blender</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<Fusion360 className="size-9 my-0.5 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">Fusion 360</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<VSCode className="size-8 my-1 text-header" />
-							<span className="text-sm text-center whitespace-nowrap">VS Code</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<AdobePhotoshop className="text-3xl mt-1" />
-							<span className="text-sm text-center whitespace-nowrap">Adobe Photoshop</span>
-						</div>
-
-						<div className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
-							<AdobeIllustrator className="text-3xl mt-1" />
-							<span className="text-sm text-center whitespace-nowrap">Adobe Illustrator</span>
-						</div>
-					</>
-				)}
+				{SKILLS_DATA[skillsTab as keyof typeof SKILLS_DATA]?.map((skill) => (
+					<div key={skill.name} className="flex flex-col items-center gap-3 p-4 border border-surface-border bg-surface rounded-xl hover:border-surface-border-hover transition">
+						{skill.icon}
+						<span className="text-sm text-center whitespace-nowrap">{skill.name}</span>
+					</div>
+				))}
 			</div>
 		</section>
 	);
--- lib/constants.tsx
+++ lib/constants.tsx
@@ -8,6 +8,26 @@ import GitHub from "@/components/icons/GitHub";
 import Slack from "@/components/icons/Slack";
 import Discord from "@/components/icons/Discord";
 import Signal from "@/components/icons/Signal";
+import ReactIcon from "@/components/icons/React";
+import Typescript from "@/components/icons/Typescript";
+import Javascript from "@/components/icons/Javascript";
+import HTML5 from "@/components/icons/HTML5";
+import CSS from "@/components/icons/CSS";
+import EJS from "@/components/icons/EJS";
+import Tailwind from "@/components/icons/Tailwind";
+import NodeJS from "@/components/icons/NodeJS";
+import Python from "@/components/icons/Python";
+import Java from "@/components/icons/Java";
+import Express from "@/components/icons/Express";
+import Prisma from "@/components/icons/Prisma";
+import Bash from "@/components/icons/Bash";
+import Arduino from "@/components/icons/Arduino";
+import Docker from "@/components/icons/Docker";
+import Blender from "@/components/icons/Blender";
+import Fusion360 from "@/components/icons/Fusion360";
+import VSCode from "@/components/icons/VSCode";
+import AdobePhotoshop from "@/components/icons/AdobePhotoshop";
+import AdobeIllustrator from "@/components/icons/AdobeIllustrator";
 
 export const HEADER_LINKS = [
 	{ label: "Home", url: "/" },
@@ -73,3 +93,34 @@ export const SOCIAL_LINKS = [
 		icon: <GitHub className="size-6" />,
 	},
 ];
+
+export const SKILLS_DATA = {
+	frontend: [
+		{ name: "React / Native", icon: <ReactIcon className="size-10 text-header" /> },
+		{ name: "Typescript", icon: <Typescript className="size-10 text-header" /> },
+		{ name: "Javascript", icon: <Javascript className="size-10 text-header" /> },
+		{ name: "HTML5", icon: <HTML5 className="size-10 text-header" /> },
+		{ name: "CSS", icon: <CSS className="size-10 text-header" /> },
+		{ name: "EJS", icon: <EJS className="size-10 text-header" /> },
+		{ name: "Tailwind CSS", icon: <Tailwind className="size-10 text-header" /> },
+	],
+	backend: [
+		{ name: "Node.js", icon: <NodeJS className="size-10 text-header" /> },
+		{ name: "Typescript", icon: <Typescript className="size-10 text-header" /> },
+		{ name: "Python", icon: <Python className="size-10 text-header" /> },
+		{ name: "Java", icon: <Java className="size-10 text-header" /> },
+		{ name: "Express", icon: <Express className="size-10 text-header" /> },
+		{ name: "Prisma", icon: <Prisma className="size-9 my-0.5 text-header" /> },
+		{ name: "Discord.js", icon: <Discord className="size-10 text-header" /> },
+	],
+	software: [
+		{ name: "Bash", icon: <Bash className="size-10 text-header" /> },
+		{ name: "Arduino", icon: <Arduino className="size-10 text-header" /> },
+		{ name: "Docker", icon: <Docker className="size-10 text-header" /> },
+		{ name: "Blender", icon: <Blender className="size-10 text-header" /> },
+		{ name: "Fusion 360", icon: <Fusion360 className="size-9 my-0.5 text-header" /> },
+		{ name: "VS Code", icon: <VSCode className="size-8 my-1 text-header" /> },
+		{ name: "Adobe Photoshop", icon: <AdobePhotoshop className="text-3xl mt-1" /> },
+		{ name: "Adobe Illustrator", icon: <AdobeIllustrator className="text-3xl mt-1" /> },
+	]
+};
